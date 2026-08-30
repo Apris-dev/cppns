28ec1fbc2f58663fab5d3398051c6bea56e14a49
masonp123 <mason@apris.dev>
2026-08-29 20:50:42 -0400

    - Removed TestBootstrapper for when bootstrapper is not enabled, added test failure if TestLib is not found.

--- packages/testing/TestExec.cpp
+++ packages/testing/TestExec.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <vector>
 
 #include "TestTrueMain.h"
 
-#ifdef USING_BOOTSTRAPPER
-EXPORTC int run() {
-#else
+#ifndef USING_BOOTSTRAPPER
 int main() {
-#endif
+    std::cout << "Bootstrapper is not enabled!" << std::endl;
+
+    return 0;
+}
+#else
+EXPORTC int run() {
 
     std::cout << "TestExec Called" << std::endl;
 
     std::cout << "Loaded Modules:" << std::endl;
 
-    for (auto& mod : getModules()) {
-        std::cout << mod << std::endl;
+    std::vector<std::string> modules = getModules();
+
+    if (std::find(modules.begin(), modules.end(), "TestLib") != modules.end()) {
+        std::cout << "Found TestLib!" << std::endl;
+        return 0;
     }
 
-    return 0;
+    std::cout << "Could not find TestLib!" << std::endl;
+    return 1;
 }
+#endif
