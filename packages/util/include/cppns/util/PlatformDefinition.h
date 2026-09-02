053ce355225d100d540a52ab73505cb8696bc5ff
masonp123 <mason@apris.dev>
2026-09-02 18:45:37 -0400

    - Seperated types and added Common.h

--- packages/util/include/cppns/util/PlatformDefinition.h
+++ packages/util/include/cppns/util/PlatformDefinition.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cstddef>
-
 /*
  * The Class that helps define platform specific things
  * For example: path separators, line endings, and the size of 'long'
@@ -72,51 +70,26 @@
 #endif
 
 #if USING_WINDOWS
-#define PATH_SEPARATOR '\\'
-#define LINE_ENDING "\r\n"
+    #define PATH_SEPARATOR '\\'
+    #define LINE_ENDING "\r\n"
 #else
-#define PATH_SEPARATOR '/'
-#define LINE_ENDING "\n"
+    #define PATH_SEPARATOR '/'
+    #define LINE_ENDING "\n"
 #endif
 
 #if USING_MSVC
-#define EXPORT __declspec(dllexport)
+    #define EXPORT __declspec(dllexport)
 #else
-#define EXPORT __attribute__((visibility("default")))
+    #define EXPORT __attribute__((visibility("default")))
 #endif
 
 #define EXPORTC extern "C" EXPORT
 
 #ifdef USING_BOOTSTRAPPER
-#define cppns_main EXPORTC int run
+    #define cppns_main EXPORTC int run
 #else
-#define cppns_main int main
+    #define cppns_main int main
 #endif
-
-#include <cstdint>
-
-using uint8 = std::uint8_t;
-using uint16 = std::uint16_t;
-using uint32 = std::uint32_t;
-using uint64 = std::uint64_t;
-using int8 = std::int8_t;
-using int16 = std::int16_t;
-using int32 = std::int32_t;
-using int64 = std::int64_t;
-using smallest = uint8;
-#if USING_WINDOWS || USING_APPLE
-using largest = size_t;
-#else
-using largest = std::size_t;
-#endif
-
-#ifdef __cpp_char8_t
-using utf8 = char8_t;
-#else
-using utf8 = char;
-#endif
-using utf16 = char16_t;
-using utf32 = char32_t;
 
 // Allows you to optionally define whether a function exists or not. Must be constexpr and static
 #define ENABLE_FUNC_IF(cond) template<bool b = cond, std::enable_if_t<b, int> = 0>
