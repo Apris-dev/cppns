ce35cd5eb5a5984b54f9053923ad1a8fb6da3618
masonp123 <mason@apris.dev>
2026-09-05 00:44:14 -0400

    - Added more version macros and corrected behavior on MSVC

--- packages/cppns/include/cppns/Common.h
+++ packages/cppns/include/cppns/Common.h
@@ -6,8 +6,22 @@
     #define cppns_main int main
 #endif
 
-#define USING_CXX20 (__cplusplus >= 202002L)
-#define USING_CXX23 (__cplusplus >= 202100L)
+// MSVC might not set __cplusplus properly
+#if defined(_MSVC_LANG) && _MSVC_LANG > __cplusplus
+#define USING_CXX11 (_MSVC_LANG > 199711L)
+#define USING_CXX14 (_MSVC_LANG > 201103L)
+#define USING_CXX17 (_MSVC_LANG > 201402L)
+#define USING_CXX20 (_MSVC_LANG > 201703L)
+#define USING_CXX23 (_MSVC_LANG > 202002L)
+#define USING_CXX26 (_MSVC_LANG > 202302L)
+#else
+#define USING_CXX11 (__cplusplus > 199711L)
+#define USING_CXX14 (__cplusplus > 201103L)
+#define USING_CXX17 (__cplusplus > 201402L)
+#define USING_CXX20 (__cplusplus > 201703L)
+#define USING_CXX23 (__cplusplus > 202002L)
+#define USING_CXX26 (__cplusplus > 202302L)
+#endif
 
 #if USING_CXX20
     #define constexpr_20 constexpr
