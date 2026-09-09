c211b9198f6414dc53e61d02f168b9fe5131f888
MasonP-Apris <mason@apris.dev>
2026-09-09 14:36:27 -0400

    - Instead disable tests when cross compiling at all without an emulator

--- scripts/packages.cmake
+++ scripts/packages.cmake
@@ -33,6 +33,7 @@ function(define_project
     message(STATUS "${PROJECT_NAME}: Using compiler ${CMAKE_CXX_COMPILER_ID} on ${CMAKE_SYSTEM}!")
 
     message(STATUS "cppns: Compiling for ${CMAKE_SYSTEM_PROCESSOR} on host ${CMAKE_HOST_SYSTEM_PROCESSOR}")
+    message(STATUS "cppns: Is Cross-Compiling: ${CMAKE_CROSSCOMPILING}")
 
     # Set the project of the current scope
     set(CURRENT_SCOPE_PROJECT ${PROJECT_NAME} PARENT_SCOPE)
