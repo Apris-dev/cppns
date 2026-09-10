050248729689cfa36192c7751199e1525ee0ae9f
masonp123 <mason@apris.dev>
2026-09-09 21:19:39 -0400

    - Added clang arm64 and renamed gcc arm64

---
+++ scripts/toolchains/clang-arm64.cmake
@@ -0,0 +1,18 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+# No extra packages should need to be installed
+set(CMAKE_C_COMPILER /bin/clang)
+set(CMAKE_CXX_COMPILER /bin/clang++)
+
+set(CMAKE_C_COMPILER_TARGET aarch64-linux-gnu)
+set(CMAKE_CXX_COMPILER_TARGET aarch64-linux-gnu)
+
+set(CMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN /usr)
+set(CMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN /usr)
+
+# Requires qemu-user-static on debian
+set(CMAKE_CROSSCOMPILING_EMULATOR
+        "/usr/bin/qemu-aarch64;-L;/usr/aarch64-linux-gnu"
+        CACHE STRING "ARM64 QEMU emulator"
+)
--- scripts/toolchains/gcc-arm64.cmake
+++ scripts/toolchains/gcc-arm64.cmake
@@ -5,6 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER /bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /bin/aarch64-linux-gnu-g++)
 
+# Requires qemu-user-static on debian
 set(CMAKE_CROSSCOMPILING_EMULATOR
         "/usr/bin/qemu-aarch64;-L;/usr/aarch64-linux-gnu"
         CACHE STRING "ARM64 QEMU emulator"
