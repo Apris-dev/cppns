4ad73f7df83cf4a862ef154696bcc2da9acc252c
masonp123 <mason@apris.dev>
2026-09-09 20:59:21 -0400

    - Updated Tests to newer format to work with emulator - Minimum required cmake is 3.29 (new emulator features) - Added linux arm64 toolchain

---
+++ scripts/toolchains/linux-arm64.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+# Requires both gcc-aarch64-linux-gnu and g++-aarch64-linux-gnu on debian
+set(CMAKE_C_COMPILER /bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /bin/aarch64-linux-gnu-g++)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR
+        "/usr/bin/qemu-aarch64;-L;/usr/aarch64-linux-gnu"
+        CACHE STRING "ARM64 QEMU emulator"
+)
